4126829bb1422e76bb592ef0e2a6ad0df4d73827
Colin Bassett <ccwbassett@gmail.com>
2021-08-27 15:47:44 -0700

    Scaffold pages

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>Blog Page!</h1>
+      <p>This is a blog page where posts will be!</p>
+    </div>
+  )
+};
+
+export default BlogPage
