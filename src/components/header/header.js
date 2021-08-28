d8f06b506a3c1aa0c573a36b19de344b68d1faad
Colin Bassett <ccwbassett@gmail.com>
2021-08-28 11:17:02 -0700

    header scss

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,15 @@
 import { Link } from 'gatsby'
 import React from 'react'
 
+import * as headerStyles from './header.module.scss';
+
 const Header = () => {
   return (
     <header>
       <h1>Header Title!</h1>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
+          <li><Link className={headerStyles.link} to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
           <li><Link to="/blog">Blog</Link></li>
