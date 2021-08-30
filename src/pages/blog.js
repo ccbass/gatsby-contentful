e90b595dd72df8d4bee404252fe802e021f10f77
Colin Bassett <ccwbassett@gmail.com>
2021-08-29 18:54:02 -0700

    contentful blog view

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,33 +8,28 @@ import * as blogStyles from './blog.module.scss'
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
         edges {
           node {
-            frontmatter {
-              title
-              date
-            }
-            fields {
-              slug
-            }
+            title
+            slug
+            publishedDate(formatString: "MMMM Do, YYYY")
           }
         }
       }
     }
   `)
 
-  const postsArray = data.allMarkdownRemark.edges.map(post => {
+  const postsArray = data.allContentfulBlogPost.edges.map((post, index) => {
     return (
-      <li className={blogStyles.post}>
-        <Link to={`/blog/${post.node.fields.slug}`}>
-          <h2>{post.node.frontmatter.title}</h2>
-          <p>{post.node.frontmatter.date}</p>
+      <li className={blogStyles.post} key={`${index}-${post.node.slug}`}>
+        <Link to={`/blog/${post.node.slug}`}>
+          <h2>{post.node.title}</h2>
+          <p>{post.node.publishedDate}</p>
         </Link>
       </li>
     )
   })
-
 
   return (
     <Layout>
