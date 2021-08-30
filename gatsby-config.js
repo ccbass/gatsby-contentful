2a0b771d3a5fcb43e11d1ab00afa48c7e5168005
Colin Bassett <ccwbassett@gmail.com>
2021-08-30 00:17:52 -0700

    contentful images working

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
       },
     },
     'gatsby-plugin-sharp',
+    `gatsby-plugin-image`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,27 +1,66 @@
 import React from "react"
-import Layout from "../components/layout/layout"
 import { graphql } from "gatsby"
 
+import { BLOCKS } from '@contentful/rich-text-types';
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+import Layout from "../components/layout/layout"
+
+
 export const query = graphql`
-  query ($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
+  query($slug: String!) {
+    blog: contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        raw
+        references {
+          ... on ContentfulAsset {
+            contentful_id
+            __typename
+            title
+            fixed(width: 750) {
+              src
+            }
+          }
+        }
       }
-      html
+    }
+    images: allContentfulAsset {
+      edges {
+        node {
+          title
+          contentful_id
+          id
+          fixed(width: 750) {
+            src
+          }
+        }
+      }
     }
   }
 `
 
 const BlogPost = ({ data }) => {
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: node => {
+        const { id } = node.data.target.sys
+        const imageNode = data.images.edges.find(image => image.node.contentful_id === id)
+
+        return <img src={imageNode.node.fixed.src} />
+      },
+    },
+  }
+
   return (
     <Layout>
-      <h1>{data.markdownRemark.frontmatter.title}</h1>
-      <p>{data.markdownRemark.frontmatter.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html}}></div>
+      <h1>{data.blog.title}</h1>
+      <p>{data.blog.publishedDate}</p>
+      {documentToReactComponents( JSON.parse(data.blog.body.raw), options )}
     </Layout>
   )
 }
 
 export default BlogPost
+
