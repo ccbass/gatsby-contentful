fc64e964709a9d0d3d7d68330650b19a09714532
Colin Bassett <ccwbassett@gmail.com>
2021-08-30 15:37:50 -0700

    use renderRichText

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,16 +1,15 @@
 import React from "react"
 import { graphql } from "gatsby"
-
 import { BLOCKS } from '@contentful/rich-text-types';
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { renderRichText } from "gatsby-source-contentful/rich-text"
 
 import Layout from "../components/layout/layout"
 import Head from "../components/head";
 
-
 export const query = graphql`
   query($slug: String!) {
-    blog: contentfulBlogPost(slug: { eq: $slug }) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      slug
       title
       publishedDate(formatString: "MMMM Do, YYYY")
       body {
@@ -20,21 +19,9 @@ export const query = graphql`
             contentful_id
             __typename
             title
-            fixed(width: 750) {
+            fixed (width: 750) {
               src
             }
-          }
-        }
-      }
-    }
-    images: allContentfulAsset {
-      edges {
-        node {
-          title
-          contentful_id
-          id
-          fixed(width: 750) {
-            src
           }
         }
       }
@@ -45,21 +32,20 @@ export const query = graphql`
 const BlogPost = ({ data }) => {
   const options = {
     renderNode: {
-      [BLOCKS.EMBEDDED_ASSET]: node => {
-        const { id } = node.data.target.sys
-        const imageNode = data.images.edges.find(image => image.node.contentful_id === id)
-
-        return <img src={imageNode.node.fixed.src} />
-      },
+      [BLOCKS.EMBEDDED_ASSET]: (node) => {
+        const {title} = node.data.target
+        const {src} = node.data.target.fixed
+        return <img src={src} alt={title}/>
+      }
     },
   }
 
   return (
     <Layout>
-      <Head title={data.blog.title} />
-      <h1>{data.blog.title}</h1>
-      <p>{data.blog.publishedDate}</p>
-      {documentToReactComponents( JSON.parse(data.blog.body.raw), options )}
+      <Head title={data.contentfulBlogPost.title} />
+      <h1>{data.contentfulBlogPost.title}</h1>
+      <p>{data.contentfulBlogPost.publishedDate}</p>
+      <div>{renderRichText(data.contentfulBlogPost.body, options)}</div>
     </Layout>
   )
 }
