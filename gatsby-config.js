325817ad1d4c0103386b9268fa8fd52842f1478b
Colin Bassett <ccwbassett@gmail.com>
2021-09-10 17:23:19 -0700

    removed remark, transformer, images, rel

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,21 +12,6 @@ module.exports = {
     'gatsby-plugin-sharp',
     `gatsby-plugin-image`,
     {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          `gatsby-remark-relative-images`,
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 750,
-              linkImagesToOriginal: false,
-            }
-          }
-        ]
-      }
-    },
-    {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
